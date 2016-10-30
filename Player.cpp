1471808f8ee902dae15c4fde6d12d691e6684424
David Egan <davideganse@gmail.com>
2016-10-30 13:37:09 -0400

    Initial project commit

---
+++ Player.cpp
@@ -0,0 +1,6 @@
+/* A Yahtzee player.  Players each have a score card */
+class Player{
+	
+
+
+};
---
+++ Yahtzee.cpp
@@ -0,0 +1,14 @@
+/* Main File */
+#include <iostream>
+
+int main(){
+	return 0;
+}
+
+void initializeGame(){
+	std::cout << "Initialize Game" << std::endl;
+}
+
+void playGame(){
+	std::cout << "Play Game" << std::endl;
+}
